1609299626479e8eceff6562b39f5f2972b563f3
LYNX <my@sfogel.ru>
2023-06-10 16:02:14 +0300

    Страница профиля свёрстана кодом, Пофикшена корректность работы зума и отображения SingleImageView

--- SingleImageViewController/SingleImageViewController.swift
+++ SingleImageViewController/SingleImageViewController.swift
@@ -15,26 +15,27 @@ final class SingleImageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.minimumZoomScale = 0.1
-        scrollView.maximumZoomScale = 1.25
+        scrollView.maximumZoomScale = 20
         imageView.image = image
         rescaleAndCenterImageInScrollView(image: image)
     }
     
     private func rescaleAndCenterImageInScrollView(image: UIImage) {
-        let minZoomScale = scrollView.minimumZoomScale
-        let maxZoomScale = scrollView.maximumZoomScale
-        view.layoutIfNeeded()
         let visibleRectSize = scrollView.bounds.size
         let imageSize = image.size
         let hScale = visibleRectSize.width / imageSize.width
         let vScale = visibleRectSize.height / imageSize.height
-        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
-        scrollView.setZoomScale(scale, animated: false)
+        let scale = min(scrollView.maximumZoomScale, max(scrollView.minimumZoomScale, min(hScale, vScale)))
+        let targetWidth = imageSize.width * scale
+        let targetHeight = imageSize.height * scale
+        imageView.frame = CGRect(x: 0, y: 0, width: targetWidth, height: targetHeight)
+        scrollView.contentSize = imageView.frame.size
+        view.layoutIfNeeded()
         scrollView.layoutIfNeeded()
-        let newContentSize = scrollView.contentSize
-        let x = (newContentSize.width - visibleRectSize.width)
-        let y = (newContentSize.height - visibleRectSize.height)
-        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+        scrollView.zoomScale = scale
+        let verticalPadding =  max(0, (scrollView.contentSize.height - scrollView.bounds.height) / 2)
+        let horizontalPadding =  max(0, (scrollView.contentSize.width - scrollView.bounds.width) / 2)
+        scrollView.contentOffset = CGPoint(x: horizontalPadding, y: verticalPadding)
     }
     
     @IBAction private func didTapBackButton() {
@@ -47,6 +48,16 @@ final class SingleImageViewController: UIViewController {
             applicationActivities: nil
         )
         present(share, animated: true, completion: nil)
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let imageViewSize = imageView.frame.size
+        let scrollViewSize = scrollView.bounds.size
+        
+        let verticalPadding = imageViewSize.height < scrollViewSize.height ? (scrollViewSize.height - imageViewSize.height) / 2 : 0
+        let horizontalPadding = imageViewSize.width < scrollViewSize.width ? (scrollViewSize.width - imageViewSize.width) / 2 : 0
+        
+        scrollView.contentInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding, bottom: verticalPadding, right: horizontalPadding)
     }
     
 }
