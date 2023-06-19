841608f6d73b819f55a76e0925059a8f97698077
LYNX <my@sfogel.ru>
2023-06-19 23:23:16 +0300

    Добавлена возможность авторизации O2Auth. Реализован KVO. Добавлена опция проверки необходимости авторизации.

---
+++ Constants.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+
+let AccessKey = "yBChinjqUPQWcwfrfNAlV64fBZ8pKDq5q4rf1lA1NeU"
+let SecretKey = "kLnc7jf7v1DbCzYL2Cg_vwkAtWohhm1uo9ZpFrOh_RU"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com/")!
